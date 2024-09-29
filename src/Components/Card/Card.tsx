b86bd61ac1aead39afe6e4032db5889e011ac836
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-09-29 11:58:51 +0700

    Finshark-4-Fix2HalfLession345

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -7,7 +7,7 @@ type Props = {
     price: number;
 }
 
-const Card = ({ companyName, ticker, price}: Props) => {
+const Card: React.FC<Props> = ({ companyName, ticker, price}: Props) : JSX.Element => {
   return <div className='card'>
     <img src="https://i.pinimg.com/originals/1c/54/f7/1c54f7b06d7723c21afc5035bf88a5ef.png" alt="Nice Image" />
     <div className='details'>
--- src/Components/CartList/CardList.tsx
+++ src/Components/CartList/CardList.tsx
@@ -3,11 +3,11 @@ import Card from '../Card/Card'
 
 interface Props {}
 
-const CardList = (props: Props) => {
+const CardList : React.FC<Props> = (props: Props) : JSX.Element => {
   return <div>
     <Card companyName='Apple' ticker='AAPL' price={100} />
     <Card companyName='MSFT' ticker='MSFT' price={200}/>
-    <Card companyName='Tesla' ticker='TSL' price={150}/>
+    <Card companyName='Tesla' ticker='TSLA' price={150}/>
   </div>
 }
 
