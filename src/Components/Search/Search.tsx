dc58c2f4d6e193c5bf90848ec58e6b2da573e360
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-10-01 22:39:52 +0700

    Finshark-6-20241001-TypeNarrowingAndDataFrom

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,20 +1,20 @@
 import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent } from 'react'
 
-type Props = {}
+//Define things this component take
+interface Props {
+  // onClick func take e param and return void
+  // Really like at TTA
+  onClick: (e: SyntheticEvent) => void
+  search: string | undefined
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
 
-const Search : React.FC<Props> = (props: Props) : JSX.Element => {
-    const [search, setSearch] = useState<string>("")
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e)
-    }
-    const onClickA = (e: SyntheticEvent) => {
-      console.log(e)
-    }
+// setup prop to pass them down : Pass function and pass value
+const Search : React.FC<Props> = ({onClick, search, handleChange}: Props) : JSX.Element => {
   return (
     <div>
     <input type="text" value={search} onChange={(e) => handleChange(e)} />
-    <button onClick={(e) => onClickA(e)} />
+    <button onClick={(e) => onClick(e)} />
     </div>
   )
 }
