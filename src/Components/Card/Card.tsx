b9a15c07755f160e2f9562aed1cb5467ef47e43b
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-10-02 22:09:03 +0700

    Finshark-7-20241002-FixConditionalRenderingAndList

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 // Quick tsrafce to make demo code
 import './Card.css'
+import { CompanySearch } from '../../company';
 type Props = {
-    companyName: string;
-    ticker: string;
-    price: number;
+    id: string;
+    searchResults: CompanySearch;
 }
 
-const Card: React.FC<Props> = ({ companyName, ticker, price}: Props) : JSX.Element => {
+const Card: React.FC<Props> = ({ id, searchResults }: Props) : JSX.Element => {
   return <div className='card'>
     <img src="https://i.pinimg.com/originals/1c/54/f7/1c54f7b06d7723c21afc5035bf88a5ef.png" alt="Nice Image" />
     <div className='details'>
-        <h2>{companyName} ({ticker})</h2>
-        <p>${price}</p>
+        <h2>{searchResults.name} ({searchResults.symbol})</h2>
+        <p>${searchResults.currency}</p>
     </div>
-    <p className='info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis deleniti quas magni expedita modi praesentium, molestiae exercitationem rem sunt natus iste asperiores, rerum numquam, similique laudantium perferendis a? Adipisci, iste.</p>
+    <p className='info'>{searchResults.exchangeShortName} - {searchResults.stockExchange}</p>
     </div>
 }
 
