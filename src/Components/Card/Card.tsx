b63e4219812cd32f26ae9f742d59d0c0456ad7a0
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-10-11 21:56:32 +0700

    Finshark-16-202410111-FixRouterWeb

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -3,6 +3,7 @@ import React, { SyntheticEvent } from 'react'
 import './Card.css'
 import { CompanySearch } from '../../company';
 import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
+import { Link } from 'react-router-dom';
 type Props = {
     id: string;
     searchResults: CompanySearch;
@@ -20,16 +21,33 @@ const Card: React.FC<Props> = ({ id, searchResults, onPortfolioCreate }: Props) 
     // <p className='info'>{searchResults.exchangeShortName} - {searchResults.stockExchange}</p>
     // <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResults.symbol} />
     // </div>
+    // <div
+    //   className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+    //   key={id}
+    //   id={id}
+    // >
+    //   <h2 className="font-bold text-center text-black md:text-left">
+    //     {searchResults.name} ({searchResults.symbol})
+    //   </h2>
+    //   <p className="text-black">{searchResults.currency}</p>
+    //   <p className="font-bold text-black">
+    //     {searchResults.exchangeShortName} - {searchResults.stockExchange}
+    //   </p>
+    //   <AddPortfolio
+    //     onPortfolioCreate={onPortfolioCreate}
+    //     symbol={searchResults.symbol}
+    //   />
+    // </div>
     <div
       className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
       key={id}
       id={id}
     >
-      <h2 className="font-bold text-center text-black md:text-left">
+      <Link to={`/company/${searchResults.symbol}`} className="font-bold text-center text-veryDarkViolet md:text-left">
         {searchResults.name} ({searchResults.symbol})
-      </h2>
-      <p className="text-black">{searchResults.currency}</p>
-      <p className="font-bold text-black">
+      </Link>
+      <p className="text-veryDarkBlue">{searchResults.currency}</p>
+      <p className="font-bold text-veryDarkBlue">
         {searchResults.exchangeShortName} - {searchResults.stockExchange}
       </p>
       <AddPortfolio
