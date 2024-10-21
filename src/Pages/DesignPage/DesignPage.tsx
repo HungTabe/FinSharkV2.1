8a20da0aab8fdf7cd1577ad503847ecd810dc0d6
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-10-21 23:04:02 +0700

    Garan11343

--- src/Pages/DesignPage/DesignPage.tsx
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Table from '../../Components/Table/Table'
 
 type Props = {}
 
 const DesignPage = (props: Props) => {
   return (
-    <div>DesignPage</div>
+    <>
+    <h1>Fincshar Design Page</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus est nulla impedit commodi ab dignissimos numquam, nemo nostrum laudantium inventore magnam similique voluptatem quasi reprehenderit quam magni in et corporis!</h2>
+    {/* Test place for table */}
+    <Table />
+    </>
   )
 }
 
