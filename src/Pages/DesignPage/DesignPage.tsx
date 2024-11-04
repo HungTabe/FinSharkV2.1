0f0428d9f0723b4a9784122c8e8effbb834e64c8
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-11-04 22:16:21 +0700

    Finshark-25-20241103-21-Company-Profile-TTM

--- src/Pages/DesignPage/DesignPage.tsx
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
 import RatioList from '../../Components/RatioList/RatioList'
+import { CompanyKeyMetrics } from '../../company'
+import { testIncomeStatementData } from './TestData'
 
 type Props = {}
+
+//Dummy data
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: CompanyKeyMetrics) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+]
 
 const DesignPage = (props: Props) => {
   return (
     <>
     <h1>Finshark Design Page</h1>
     <h2>This is finshark design page. This place will hold design</h2>
-    <RatioList/>
+    <RatioList data={testIncomeStatementData} config={tableConfig} />
     <Table/>
     </>
 )
