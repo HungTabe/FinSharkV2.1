b03690944dcda02225a7087bbc3126e94caf6136
Tran Dinh Hung <trandinhhung717@gmail.com>
2024-11-05 23:28:59 +0700

    X

--- src/Components/CompanyProfile/CompanyProfile.tsx
+++ src/Components/CompanyProfile/CompanyProfile.tsx
@@ -26,61 +26,43 @@ type Props = {}
     const tableConfig = [
       {
         label: "Market Cap",
-        render: (company: CompanyKeyMetrics) => company.marketCapTTM,
-        subTitle: "Total value of all a company's shares of stock",
+        render: (company: CompanyKeyMetrics) => company.marketCapTTM
       },
       {
         label: "Current Ratio",
-        render: (company: CompanyKeyMetrics) => company.currentRatioTTM,
-        subTitle:
-          "Measures the companies ability to pay short term debt obligations",
+        render: (company: CompanyKeyMetrics) => company.currentRatioTTM
       },
       {
         label: "Return On Equity",
-        render: (company: CompanyKeyMetrics) => company.roeTTM,
-        subTitle:
-          "Return on equity is the measure of a company's net income divided by its shareholder's equity",
+        render: (company: CompanyKeyMetrics) => company.roeTTM
       },
       {
         label: "Return On Assets",
-        render: (company: CompanyKeyMetrics) => company.returnOnTangibleAssetsTTM,
-        subTitle:
-          "Return on assets is the measure of how effective a company is using its assets",
+        render: (company: CompanyKeyMetrics) => company.returnOnTangibleAssetsTTM
       },
       {
         label: "Free Cashflow Per Share",
-        render: (company: CompanyKeyMetrics) => company.freeCashFlowPerShareTTM,
-        subTitle:
-          "Return on assets is the measure of how effective a company is using its assets",
+        render: (company: CompanyKeyMetrics) => company.freeCashFlowPerShareTTM      
       },
       {
         label: "Book Value Per Share TTM",
-        render: (company: CompanyKeyMetrics) => company.bookValuePerShareTTM,
-        subTitle:
-          "Book value per share indicates a firm's net asset value (total assets - total liabilities) on per share basis",
+        render: (company: CompanyKeyMetrics) => company.bookValuePerShareTTM
       },
       {
         label: "Divdend Yield TTM",
-        render: (company: CompanyKeyMetrics) => company.dividendYieldTTM,
-        subTitle: "Shows how much a company pays each year relative to stock price",
+        render: (company: CompanyKeyMetrics) => company.dividendYieldTTM
       },
       {
         label: "Capex Per Share TTM",
-        render: (company: CompanyKeyMetrics) => company.capexPerShareTTM,
-        subTitle:
-          "Capex is used by a company to aquire, upgrade, and maintain physical assets",
+        render: (company: CompanyKeyMetrics) => company.capexPerShareTTM
       },
       {
         label: "Graham Number",
-        render: (company: CompanyKeyMetrics) => company.grahamNumberTTM,
-        subTitle:
-          "This is the upperbouind of the price range that a defensive investor should pay for a stock",
+        render: (company: CompanyKeyMetrics) => company.grahamNumberTTM
       },
       {
         label: "PE Ratio",
-        render: (company: CompanyKeyMetrics) => company.peRatioTTM,
-        subTitle:
-          "This is the upperbouind of the price range that a defensive investor should pay for a stock",
+        render: (company: CompanyKeyMetrics) => company.peRatioTTM
       },
     ];
 
--- src/Pages/DesignPage/DesignPage.tsx
+++ src/Pages/DesignPage/DesignPage.tsx
@@ -2,25 +2,34 @@ import React from 'react'
 import Table from '../../Components/Table/Table'
 import RatioList from '../../Components/RatioList/RatioList'
 import { CompanyKeyMetrics } from '../../company'
-import { testIncomeStatementData } from './TestData'
+import { TestDataCompany, testIncomeStatementData } from './TestData'
 
 type Props = {}
 
+const data = TestDataCompany;
+
+
 //Dummy data
+// const tableConfig = [
+//   {
+//     label: "Market Cap",
+//     render: (company: CompanyKeyMetrics) => company.marketCapTTM,
+//     subTitle: "Total value of all a company's shares of stock",
+//   },
+// ]
 const tableConfig = [
   {
-    label: "Market Cap",
-    render: (company: CompanyKeyMetrics) => company.marketCapTTM,
-    subTitle: "Total value of all a company's shares of stock",
+    label: "symbol",
+    render: (company: any) => company.symbol,
   },
-]
+];
 
 const DesignPage = (props: Props) => {
   return (
     <>
     <h1>Finshark Design Page</h1>
     <h2>This is finshark design page. This place will hold design</h2>
-    <RatioList data={testIncomeStatementData} config={tableConfig} />
+    <RatioList data={data} config={tableConfig} />
     <Table/>
     </>
 )
